5c6cf7683470ba0098994e21b1a1f315b54aff8b
mbblau423 <max.blau@gmail.com>
2018-05-30 10:24:30 -0700

    Updated blender script and blender model. Need background imgs

--- blenderscript.py
+++ blenderscript.py
@@ -1,36 +1,42 @@
 import bpy
 import mathutils
+import json
+import numpy as np
 from math import radians
 from os.path import join
+import os
 
 S = bpy.context.scene
 
 renderFolder = "D:/MyRenderFolder/"
 logFile = "D:/MyRenderFolder/logs.txt"
 
+for the_file in os.listdir(renderFolder):
+    file_path = os.path.join(renderFolder, the_file)
+    try:
+        if os.path.isfile(file_path):
+            os.unlink(file_path)
+    except Exception as e:
+        print(e)
+        exit()
+
 obj = bpy.data.objects["model"]
 cam = bpy.data.objects["Camera"]
 
-numAngles = 4
+numAngles = 30
 rotAngle  = 360 / numAngles
-
-origCamPos = cam.location
-camPosArr = []
+vertRotAngle = 180 / numAngles
 
 for i in range(numAngles):
-    for j in range(-5, 5):
-        deltaY = j * 0.1 + origCamPos.y
-        camPosArr.append(deltaY)
-        cam.location = mathutils.Vector((origCamPos.x, deltaY, origCamPos.z))
+    for j in range(int(-numAngles/2)+1, int(numAngles/2)+1):
+        vertAngle = j * vertRotAngle
+        obj.rotation_euler.y = radians( vertAngle )
 
         angle = i * rotAngle
         obj.rotation_euler.z = radians( angle )
 
-        fileName = "angle_{a}_cam_{f}".format( a = angle, f = j )
+        fileName = "angle_{a}_cam_{f}".format( a = angle, f = vertAngle)
         fileName += S.render.file_extension
         bpy.context.scene.render.filepath = join( renderFolder, fileName )
 
         bpy.ops.render.render(write_still = True)
-
-with open(logFile, 'w') as f:
-    f.write(camPosArr)
